3995adcaabe2133657e8e99e64d01d7bd5d6f203
Mario P. <contact@marioparaschiv.com>
2026-06-17 18:56:55 +0100

    Use seconds for Node stream timeouts

--- bindings/nodejs/lib/index.js
+++ bindings/nodejs/lib/index.js
@@ -2758,7 +2758,7 @@ class Session {
    * @param {Object} [options.params] - URL query parameters
    * @param {Object} [options.headers] - Request headers
    * @param {Object} [options.cookies] - Cookies to send
-   * @param {number} [options.timeout] - Request timeout in milliseconds
+   * @param {number} [options.timeout] - Request timeout in seconds
    * @returns {StreamResponse} - Streaming response for chunked reading
    *
    * Example:
@@ -2794,7 +2794,7 @@ class Session {
       reqOptions.headers = mergedHeaders;
     }
     if (timeout) {
-      reqOptions.timeout = timeout;
+      reqOptions.timeout = timeout * 1000;
     }
     if (allowRedirects !== null && allowRedirects !== undefined) {
       reqOptions.follow_redirects = !!allowRedirects;
@@ -2843,7 +2843,7 @@ class Session {
    * @param {Object} [options.params] - URL query parameters
    * @param {Object} [options.headers] - Request headers
    * @param {Object} [options.cookies] - Cookies to send
-   * @param {number} [options.timeout] - Request timeout in milliseconds
+   * @param {number} [options.timeout] - Request timeout in seconds
    * @returns {StreamResponse} - Streaming response for chunked reading
    */
   postStream(url, options = {}) {
@@ -2885,7 +2885,7 @@ class Session {
       reqOptions.headers = mergedHeaders;
     }
     if (timeout) {
-      reqOptions.timeout = timeout;
+      reqOptions.timeout = timeout * 1000;
     }
     if (allowRedirects !== null && allowRedirects !== undefined) {
       reqOptions.follow_redirects = !!allowRedirects;
